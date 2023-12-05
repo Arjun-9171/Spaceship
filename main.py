d3bb81ce806c159d46eb762e3bae036b62a93283
Arjun-9171 <arjunanand923@gmail.com>
2023-12-04 19:02:56 -0800

    Enemy spaceship added to game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 pygame.init()
 screen_width = 1280
@@ -8,24 +8,45 @@ display_screen = pygame.display.set_mode((screen_width, screen_height))
 lazer = pygame.image.load('Lazer.png')
 lazer = pygame.transform.scale(lazer, (30, 30))
 lazer = pygame.transform.rotate(lazer, -90)
+
+#Break
+
+enemy_spaceship = pygame.image.load('Enemy_spaceship.gif')
+enemy_spaceship = pygame.transform.scale(enemy_spaceship, (145, 145))
+enemy_spaceship = pygame.transform.rotate(enemy_spaceship, 90)
+enemy_x_pos = 900
+enemy_y_pos = 300
+#Break
+
 background = pygame.image.load('bg.jpg')
 background = pygame.transform.scale(background, (screen_width, screen_height))
+
+#Break
+
 spaceship = pygame.image.load('Spaceship.png')
 spaceship = pygame.transform.scale(spaceship, (100, 100))
 spaceship = pygame.transform.rotate(spaceship, -90)
 spaceship_x_pos = 200
 spaceship_y_pos = 300
+
+#Break
+
 lazer_x_pos = 245
 lazer_y_pos = 335
 lazer_speed = 0
 triggered = False
+
+def respawn_enemy():
+  enemy_y_pos = random.randint(1, 715)
+  enemy_x_pos = 1300
+  return[enemy_y_pos, enemy_x_pos]
 
 def respawn_lazer():
   triggered = False
   respawn_lazer_x = spaceship_x_pos
   respawn_lazer_y = spaceship_y_pos
   lazer_speed = 0
-  return[respawn_lazer_x, respawn_lazer_y, triggered, lazer_speed]
+  return[triggered, respawn_lazer_x, respawn_lazer_y, lazer_speed]
 
 
   
@@ -56,16 +77,23 @@ while run:
 
   if key[pygame.K_SPACE]:
     triggered = True
-    lazer_x_pos = lazer_x_pos + 15
+    lazer_speed = 10
 
-  if lazer_x_pos == 1300:
-    respawn_lazer_x, respawn_lazer_y, triggered, lazer_speed = respawn_lazer()
-
-  screen_width = screen_width - 2
-  lazer_x_pos = lazer_x_pos + lazer_speed
+  if lazer_speed > 1300:
+    triggered, respawn_lazer_x, respawn_lazer_y, lazer_speed = respawn_lazer()
   
-  display_screen.blit(lazer, (lazer_x_pos, lazer_y_pos))
+  if enemy_x_pos < 0:
+    enemy_y_pos = respawn_enemy()[0]
+    enemy_x_pos = respawn_enemy()[1]
+
+  screen_width = screen_width - 0.7
+  lazer_x_pos = lazer_x_pos + lazer_speed
+  enemy_x_pos = enemy_x_pos - 1
+
   display_screen.blit(spaceship, (spaceship_x_pos, spaceship_y_pos))
+  display_screen.blit(lazer, (lazer_x_pos, lazer_y_pos))
+  display_screen.blit(enemy_spaceship, (enemy_x_pos, enemy_y_pos))
+ 
 
   
   pygame.display.update()
